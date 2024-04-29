ef955f3524e7717d5666616eed43ab5a7002fd12
A-20 <A-20@A-20>
2024-04-29 10:54:39 +0700

    bab 3 & bab4

--- mavenproject1/src/main/java/bab3/NewClass.java
+++ mavenproject1/src/main/java/bab3/NewClass.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package newpackage.inputdata;
+package bab3;
 import java.util.Scanner;
 /**
  *
